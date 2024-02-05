bf44dc2be6101e249bcb26470a4d17eb3feb85b5
AlbertNg <albert3005@gmail.com>
2024-02-04 21:50:38 -0500

    UserPrefs Edits

---
+++ private_dot_emacs.d/elpa/29.2/develop/spacemacs-purpose-popwin-20240204.135317/spacemacs-purpose-popwin-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from spacemacs-purpose-popwin.el  -*- no-byte-compile: t -*-
+(define-package "spacemacs-purpose-popwin" "20240204.135317" "Purpose extension to act like Popwin" 'nil)
---
+++ private_dot_emacs.d/elpa/29.2/develop/vim-powerline-20240204.135305/vim-powerline-pkg.el
@@ -0,0 +1,4 @@
+(define-package "vim-powerline" "20240204.135305" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
