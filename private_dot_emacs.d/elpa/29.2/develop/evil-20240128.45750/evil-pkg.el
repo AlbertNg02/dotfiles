cd6bc7c34eeba0e6ef3028ae499022da6f2de162
Albert Hoang Nguyen <77342590+AlbertNg02@users.noreply.github.com>
2024-02-13 22:43:29 +0000

    Remove Clutter

--- private_dot_emacs.d/elpa/29.2/develop/evil-20240128.45750/evil-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "evil" "20240128.45750" "Extensible Vi layer for Emacs."
-  '((emacs "24.1")
-    (goto-chg "1.6")
-    (cl-lib "0.5")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- private_dot_emacs.d/elpa/29.2/develop/vim-powerline-20240204.135305/vim-powerline-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "vim-powerline" "20240204.135305" "No description available." 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
