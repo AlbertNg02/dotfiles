e0bc1905b1e845ff72fe6cb1984779a01d6b79f3
albert <albert.ng.hoang@gmail.com>
2024-05-04 14:38:09 -0500

    added plugsins

--- private_dot_config/nvim/lua/community.lua
+++ private_dot_config/nvim/lua/community.lua
@@ -15,13 +15,13 @@ return {
   { import = "astrocommunity.scrolling.neoscroll-nvim" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.marks-nvim" },
+  { import = "astrocommunity.motion.harpoon" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.markdown-and-latex.peek-nvim" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
 
   -- Git and overview
   { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.remote-development.distant-nvim" },
   { import = "astrocommunity.project.project-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.git.fugit2-nvim" },
--- private_dot_config/nvim/lua/plugins/user.lua
+++ private_dot_config/nvim/lua/plugins/user.lua
@@ -16,6 +16,7 @@ return {
       require("orgmode").setup {
         org_agenda_files = "~/orgfiles/**/*",
         org_default_notes_file = "~/orgfiles/refile.org",
+        org_startup_folded = "inherit",
       }
 
       -- NOTE: If you are using nvim-treesitter with `ensure_installed = "all"` option
